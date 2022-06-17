1bf80b94b2c88e1462e17402abfacba27da9d471
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-17 14:21:17 +0900

    add battle

--- src/character/App.java
+++ src/character/App.java
@@ -1,5 +1,7 @@
 package character;
 
+import monster.Monster;
+
 public class App {
 
 	public static void main(String[] args) {
@@ -10,6 +12,19 @@ public class App {
 		Warrior warrior = new Warrior("ボブ");
 		warrior.greet("どうも!");
 		warrior.showStatus();
+		
+		//ボブがモンスターに攻撃する
+		Monster monster = new Monster();
+		warrior.attack(monster);
+//		wizard.attack(monster);
+		
+		//モンスターが生きているか？確認
+		System.out.println(monster.hp);
+		if (monster.isAlive()) {
+			System.out.println(monster.name + "は生きている");
+		} else {
+			System.out.println(monster.name + "を倒した！");
+		}
 	}
 
 }

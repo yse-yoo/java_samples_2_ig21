b94d2606bd8eefc836fac4f9948f20bd3e13ce74
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-17 09:37:28 +0900

    add Character

--- src/character/Character.java
+++ src/character/Character.java
@@ -1,5 +1,22 @@
 package character;
 
 public class Character {
+	//フィールド（プロパティ）
+	public String name;
+	public String job;
+	public int hp;
+	public int mp;
+	public int level;
+	public int exp;
+	public int attackPower;
+	public int defencePower;
 
+	//インスタンスメソッド
+	public void greet(String message) {
+		System.out.println(message);
+	}
+	
+	public void showStatus() {
+		System.out.println("Name: " + this.name);
+	}
 }

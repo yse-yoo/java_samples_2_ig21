43ab3a0f9a4cb5be7038f5b3d2bba17ad05d44e5
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-17 11:05:08 +0900

    add Animal

---
+++ src/zoo/Animal.java
@@ -0,0 +1,22 @@
+package zoo;
+
+public class Animal {
+
+	public String type;
+	public String name;
+	public String crying;
+	
+
+	public void walk() {
+		System.out.println(this.name + "が歩いた");
+	}
+	
+	public void cry() {
+		System.out.println(this.crying);
+	}
+
+	public void escape() {
+		System.out.println(this.name + "が逃げた");
+	}
+	
+}

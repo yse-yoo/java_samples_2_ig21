8805a8ece3bb759fec145722358ddd1d8e5cc345
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-17 10:45:55 +0900

    add subclass property

--- src/character/Warrior.java
+++ src/character/Warrior.java
@@ -2,8 +2,14 @@ package character;
 
 public class Warrior extends Character {
 
+	//コンストラクタ
 	public Warrior(String name) {
 		super(name);
+		this.job = "warrior";
+		this.hp = 30;
+		this.mp = 0;
+		this.attackPower = 10;
+		this.defencePower = 5;
 	}
 
 }
--- src/character/Wizard.java
+++ src/character/Wizard.java
@@ -2,10 +2,16 @@ package character;
 
 //サブクラス
 public class Wizard extends Character {
-
+	
+	//コンストラクタ
 	public Wizard(String name) {
 		//スーパークラスのコンストラクタを実行
 		super(name);
+		this.job = "wizard";
+		this.hp = 10;
+		this.mp = 20;
+		this.attackPower = 3;
+		this.defencePower = 5;
 	}
 
 }

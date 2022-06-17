b461a3b8f6415e734a7deecba37d06e3e297d277
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-17 10:00:59 +0900

    add App

---
+++ src/character/App.java
@@ -0,0 +1,13 @@
+package character;
+
+public class App {
+
+	public static void main(String[] args) {
+		Wizard wizard = new Wizard();
+		wizard.greet("こんにちは！");
+		
+		Warrior warrior = new Warrior();
+		warrior.greet("どうも!");
+	}
+
+}

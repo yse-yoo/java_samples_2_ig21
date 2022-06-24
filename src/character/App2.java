6531c64d1a9ab6b1bfc3431a84a09113882e4767
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-24 13:15:09 +0900

    add ArrayList

---
+++ src/character/App2.java
@@ -0,0 +1,12 @@
+package character;
+
+import java.util.ArrayList;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		// ArrayList
+		ArrayList<Character> characterList = new ArrayList<>();
+	}
+
+}

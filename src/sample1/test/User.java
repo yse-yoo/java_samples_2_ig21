c1f56fb0cb7b3bd115844627a37c80582a9b913a
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-24 09:25:57 +0900

    add test User

---
+++ src/sample1/test/User.java
@@ -0,0 +1,5 @@
+package sample1.test;
+
+public class User {
+
+}
--- src/sample3/App.java
+++ src/sample3/App.java
@@ -7,6 +7,7 @@ public class App {
 	public static void main(String[] args) {
 		sample1.User user1 = new sample1.User();
 		sample2.User user2 = new sample2.User();
+		sample1.test.User user3 = new sample1.test.User();
 	}
 
 }

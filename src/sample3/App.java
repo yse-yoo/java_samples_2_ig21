fd17ec6e310e4150a0eeb6b5d2bc0b6bfd98aeaa
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-24 09:23:27 +0900

    add User class

---
+++ src/sample3/App.java
@@ -0,0 +1,12 @@
+package sample3;
+
+//import sample1.User;
+
+public class App {
+
+	public static void main(String[] args) {
+		sample1.User user1 = new sample1.User();
+		sample2.User user2 = new sample2.User();
+	}
+
+}

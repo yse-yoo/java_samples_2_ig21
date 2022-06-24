6d5e52bd027561df0e2ee3ea2e4f7afc516dc665
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-24 10:13:22 +0900

    add override attack implements

--- src/character/Warrior.java
+++ src/character/Warrior.java
@@ -1,5 +1,7 @@
 package character;
 
+import java.util.Random;
+
 import monster.Monster;
 
 public class Warrior extends Character {
@@ -18,7 +20,24 @@ public class Warrior extends Character {
 	@Override
 	public int attack(Monster monster) {
 		System.out.println("オーバーライド");
-		return 0;
+		
+		// 元の攻撃力を保存
+		int attackPower = this.attackPower;
+
+		// 1/5 の確率で攻撃力を２倍
+		Random rand = new Random();
+		int randomIndex = rand.nextInt(5);
+		if (randomIndex == 0) {
+			this.attackPower *= 2;
+		}
+
+		//スーパークラスのattack() を実行
+		int damage = super.attack(monster);
+
+		//攻撃力を元に戻す
+		this.attackPower = attackPower;
+		
+		return damage;
 	}
 
 }

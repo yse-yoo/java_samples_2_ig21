8659dd75e15a186cf6689959176146832a47e200
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-24 13:30:28 +0900

    add method

--- src/character/App2.java
+++ src/character/App2.java
@@ -7,6 +7,12 @@ public class App2 {
 	public static void main(String[] args) {
 		// ArrayList
 		ArrayList<Character> characterList = new ArrayList<>();
+		
+		// Create: add()
+		characterList.add(new Wizard("アリス"));
+		characterList.add(new Wizard("テリー"));
+		characterList.add(new Warrior("ボブ"));
+		
 	}
 
 }

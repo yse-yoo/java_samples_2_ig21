560e2ce6b7e69dbecd711d429a7834a2f2332308
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-24 10:47:41 +0900

    add foreach

--- src/character/Battle.java
+++ src/character/Battle.java
@@ -1,13 +1,24 @@
 package character;
 
+import monster.Monster;
+
 public class Battle {
 
 	public static void main(String[] args) {
 		Wizard wizard1 = new Wizard("アリス");
 		Wizard wizard2 = new Wizard("テリー");
-		Warrior warrior1 = new Warrior("テリー");
+		Warrior warrior1 = new Warrior("ボブ");
 		
 		Character[] characters = { wizard1, wizard2, warrior1 };
+		
+		Monster monster1 = new Monster();
+
+		//キャラクターの人数だけ攻撃
+		for (Character character : characters) {
+			System.out.println(character.name + "の攻撃！");
+			character.attack(monster1);
+			System.out.println(monster1.isAlive());
+		}
 	}
 
 }

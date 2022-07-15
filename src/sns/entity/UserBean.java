d344c6e59300582c2babe41a768fa60c0fee1204
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-07-15 09:43:21 +0900

    add name,password

--- src/sns/entity/UserBean.java
+++ src/sns/entity/UserBean.java
@@ -7,13 +7,32 @@ public class UserBean {
 //	String email;
 
 	private String email;
+	private String name;
+	private String password;
 
+	//アクセサメソッド（setter/getter）
 	public String getEmail() {
 		return email;
 	}
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	

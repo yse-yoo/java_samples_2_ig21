49a2b0e3cf09c95b88af0a3047d2b781c54c4523
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-07-15 09:52:35 +0900

    update setter/getter

--- src/sns/entity/App.java
+++ src/sns/entity/App.java
@@ -9,9 +9,17 @@ public class App {
 
 		//setterを利用
 		user.setEmail("test@example.com");
+		user.setName("横浜　太郎");
+		user.setPassword("pass1234");
 
 		//getterを利用
 		String email = user.getEmail();
 		System.out.println(email);
+
+		String name = user.getName();
+		System.out.println(name);
+
+		String password = user.getPassword();
+		System.out.println(password);
 	}
 }

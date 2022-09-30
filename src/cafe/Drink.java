d78659e1ab51b7995b216aa462306082adbda65d
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-09-30 09:32:21 +0900

    add constructor

--- src/cafe/Drink.java
+++ src/cafe/Drink.java
@@ -5,4 +5,11 @@ public class Drink {
 	public String name;
 	public Integer price;
 	
+	public Drink(String name, Integer price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+	
+	
 }

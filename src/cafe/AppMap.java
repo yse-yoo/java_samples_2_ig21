c950b51d5c7e052f473fe51f0718268204eb9459
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-09-30 09:56:17 +0900

    add data

--- src/cafe/AppMap.java
+++ src/cafe/AppMap.java
@@ -7,7 +7,11 @@ public class AppMap {
 
 	public static void main(String[] args) {
         Map<String, Drink> drinkMap = new HashMap<>(); 
-		
+        drinkMap.put("D0001", new Drink("コーヒー", 350));
+        drinkMap.put("D0002", new Drink("紅茶", 400));
+        drinkMap.put("D0003", new Drink("ほうじ茶", 300));
+        
+        
 	}
 
 }

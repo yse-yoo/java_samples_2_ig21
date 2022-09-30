cfbfb678e0d46e0d3f85aeaaf72eed2637dfc6e0
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-09-30 14:18:05 +0900

    add calendar

--- src/cafe/AppIterator.java
+++ src/cafe/AppIterator.java
@@ -1,7 +1,11 @@
 package cafe;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Map;
 
 public class AppIterator {
 
@@ -12,7 +16,35 @@ public class AppIterator {
 		drinkList.add(new Drink("ほうじ茶", 300));
 
 		//イテレーター
+		System.out.println("--- iterator ---");
 		Iterator<Drink> iterator = drinkList.iterator();
+		while (iterator.hasNext()) {
+			Drink drink = iterator.next();
+			System.out.println(drink.name);
+			System.out.println(drink.price);
+//			System.out.println(iterator.next().name);
+//			System.out.println(iterator.next().price);
+		}
+		
+		System.out.println("--- list iterator ---");
+		ListIterator<Drink> listIterator = drinkList.listIterator();
+		listIterator.add(new Drink("カフェモカ", 350));
+		listIterator.previous();
+
+		while (listIterator.hasNext()) {
+			Drink drink = listIterator.next();
+			System.out.println(drink.name);
+		}
+		
+		// 曜日
+		Map<Integer, String> weekdays = new HashMap<>();
+		weekdays.put(Calendar.SUNDAY, "日");
+		weekdays.put(Calendar.MONDAY, "月");
+		weekdays.put(Calendar.TUESDAY, "火");
+		weekdays.put(Calendar.WEDNESDAY, "水");
+		weekdays.put(Calendar.THURSDAY, "木");
+		weekdays.put(Calendar.FRIDAY, "金");
+		weekdays.put(Calendar.SATURDAY, "土");
 
 	}
 

31245665f73cafeca6ad62deb6e4df6901a42d3b
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-09-30 09:41:29 +0900

    add map

---
+++ src/cafe/AppMap.java
@@ -0,0 +1,13 @@
+package cafe;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AppMap {
+
+	public static void main(String[] args) {
+        Map<String, Drink> drinkMap = new HashMap<>(); 
+		
+	}
+
+}

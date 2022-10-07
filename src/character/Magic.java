517e1fc896e6fccbbc86012e25df85de928992c8
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-07 11:36:41 +0900

    update type

---
+++ src/character/Magic.java
@@ -0,0 +1,17 @@
+package character;
+
+public class Magic {
+
+    public MagicType type;
+    public String name;
+    public Integer value;
+
+    public Magic(MagicType type, String name, Integer value) {
+        super();
+        this.type = type;
+        this.name = name;
+        this.value = value;
+    }
+
+    
+}

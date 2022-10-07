f8c22d10d69945b49a1ad67dd218383cf751a875
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-07 11:12:19 +0900

    Size enum

--- src/cafe/Size.java
+++ src/cafe/Size.java
@@ -1,5 +1,15 @@
 package cafe;
 
 public enum Size {
-    LARGE, MEDIUM, SMALL
+    LARGE("L"), MEDIUM("M"), SMALL("S");
+
+    private String value;
+    
+    private Size(String value) {
+        this.value = value;
+    }
+    
+    public String getValue() {
+        return this.value;
+    }
 }

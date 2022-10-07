787963918de1730b0a42e1c23055755310643c98
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-07 10:15:54 +0900

    add q3

---
+++ src/cafe/Score.java
@@ -0,0 +1,13 @@
+package cafe;
+
+public class Score {
+    public String name;
+    public Integer value;
+
+    public Score(String name, Integer value) {
+        super();
+        this.name = name;
+        this.value = value;
+    }
+    
+}

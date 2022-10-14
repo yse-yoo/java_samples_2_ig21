6f495e35edcb75a09941fb0ee16791f8758facad
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-14 11:22:16 +0900

    add synchronized

--- src/thread/AppCounter.java
+++ src/thread/AppCounter.java
@@ -4,7 +4,14 @@ public class AppCounter {
 
     public static void main(String[] args) {
         CounterThread counter = new CounterThread();
+        Thread thread1 = new Thread(counter);
+        Thread thread2 = new Thread(counter);
+        thread1.setName("スレッド1");
+        thread2.setName("スレッド2");
         
+        thread1.start();
+        thread2.start();
+
     }
 
 }

e44431d7e912ee15c60bdfd63850db735fb2ddba
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-14 11:03:44 +0900

    add CounterThread

---
+++ src/thread/AppCounter.java
@@ -0,0 +1,10 @@
+package thread;
+
+public class AppCounter {
+
+    public static void main(String[] args) {
+        CounterThread counter = new CounterThread();
+        
+    }
+
+}
---
+++ src/thread/CounterThread.java
@@ -0,0 +1,23 @@
+package thread;
+
+public class CounterThread implements Runnable {
+
+    public static Integer count = 5;
+    public long ms = 1000;
+
+    @Override
+    public void run() {
+        while (count > 0) {
+            try {
+                Thread.sleep(ms);
+                System.out.println(Thread.currentThread().getName() + ":" + count);
+                count--;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        count = 5;
+        System.out.println("--- thread end ---");
+    }
+
+}

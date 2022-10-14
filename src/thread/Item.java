3434b8499d16cc2a95106f232031be1264777d31
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-14 09:44:50 +0900

    add thread sample

---
+++ src/thread/Item.java
@@ -0,0 +1,20 @@
+package thread;
+
+public class Item {
+
+    public String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+    
+    public void order() {
+        try {
+            System.out.println(name);
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.err.println("---注文完了---");
+    }
+}

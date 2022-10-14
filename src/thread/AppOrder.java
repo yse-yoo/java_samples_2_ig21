41263a05e681484407c121ff8329b6931e467a66
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-14 10:12:19 +0900

    add ShopThread

--- src/thread/AppOrder.java
+++ src/thread/AppOrder.java
@@ -1,9 +1,19 @@
 package thread;
 
+import java.util.ArrayList;
+
 public class AppOrder {
 
     public static void main(String[] args) {
-
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Item("コーヒー"));
+        items.add(new Item("紅茶"));
+        items.add(new Item("ほうじ茶"));
+        
+        for (Item item : items) {
+            ShopThread thread = new ShopThread(item);
+            thread.start();
+        }
     }
 
 }
--- src/thread/ShopThread.java
+++ src/thread/ShopThread.java
@@ -1,9 +1,15 @@
 package thread;
 
 public class ShopThread extends Thread {
+    
+    private Item item;
+
+    public ShopThread(Item item) {
+        this.item = item;
+    }
 
     public void run() {
-        
+        item.order();
     }
 
 }

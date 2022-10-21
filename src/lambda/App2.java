cf72f87b96d3d205a1f90ab042244af9f3ee47dd
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-21 09:45:07 +0900

    add calculate method

--- src/lambda/App2.java
+++ src/lambda/App2.java
@@ -1,9 +1,23 @@
 package lambda;
 
 public class App2 {
+    public static Float TAX_RATE = 0.1f;
+    
 
     public static void main(String[] args) {
 
+        ICalculate calculate = new ICalculate() {
+            
+            @Override
+            public Integer totalPrice(Integer price) {
+                Float totalPrice = price * TAX_RATE;
+                return totalPrice.intValue();
+            }
+        };
+        
+        Integer price = calculate.totalPrice(1000);
+        System.out.println(price);
     }
+
 
 }

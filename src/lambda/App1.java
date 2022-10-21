1633397585f9d1b24c764595d4f774aaa4f9f49b
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-21 09:21:42 +0900

    add lambda

---
+++ src/lambda/App1.java
@@ -0,0 +1,17 @@
+package lambda;
+
+public class App1 {
+
+    public static void main(String[] args) {
+        Runnable runner = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello!!!!");
+            }
+            
+        };
+        
+        runner.run();
+    }
+
+}

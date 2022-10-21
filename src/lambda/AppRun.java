24bcee73fe15db4e5fcc02f25fc1ad0cfc3edcbc
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-21 10:15:12 +0900

    add methods

--- src/lambda/AppRun.java
+++ src/lambda/AppRun.java
@@ -6,14 +6,23 @@ public class AppRun {
 
         Runnable runner = () -> System.out.println("Hello!!!");
         runner.run();
-        
+
+        hello(() -> showMessage("Hello!!!"));
+        hello(() -> calculate(250, 7));
     }
     
     public static void showMessage(String message) {
         System.out.println(message);
     }
 
+    public static void calculate(Integer price, Integer amount) {
+        Integer totalPrice = price * amount;
+        System.out.println(totalPrice);
+    }
+
     public static void hello(Runnable runner) {
+       System.out.println("start");
        runner.run(); 
+       System.out.println("end");
     }
 }

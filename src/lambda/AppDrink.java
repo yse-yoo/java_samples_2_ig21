88b58ad5c07b980e6253bb0c3cbc3b98637ceeeb
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-21 10:44:49 +0900

    add DrinkApp

---
+++ src/lambda/AppDrink.java
@@ -0,0 +1,18 @@
+package lambda;
+
+import java.util.ArrayList;
+
+import cafe.Drink;
+
+public class AppDrink {
+
+    public static void main(String[] args) {
+        ArrayList<Drink> drinkList = new ArrayList<>();
+        drinkList.add(new Drink("コーヒー", 350));
+        drinkList.add(new Drink("紅茶", 400));
+        drinkList.add(new Drink("ほうじ茶", 300));
+        
+        
+    }
+
+}
